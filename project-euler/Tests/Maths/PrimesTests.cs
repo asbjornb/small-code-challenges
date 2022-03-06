aaf6b55291a2271988222befc007f6ec69343d31
Asbjørn Brandt <asb@lindcapital.com>
2022-03-06 21:57:43 +0100

    Add some base prime functionality

---
+++ project-euler/Tests/Maths/PrimesTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using project_euler.Maths;
+using Shouldly;
+using System.Linq;
+
+namespace Tests.Maths
+{
+    [TestFixture]
+    internal class PrimesTests
+    {
+        [Test]
+        [TestCase(2, new int[] { })]
+        [TestCase(3, new int[] { 2 })]
+        [TestCase(4, new int[] { 2, 3 })]
+        [TestCase(20, new int[] { 2, 3, 5, 7, 11, 13, 17, 19 })]
+        public void ShouldGeneratePrimesBelowForExamples(int input, int[] expectedResult)
+        {
+            var sut = new Primes();
+
+            var primes = sut.GeneratePrimesBelow(input);
+
+            primes.ShouldBe(expectedResult.ToList());
+        }
+
+        [Test]
+        public void ShouldGeneratePrimesBelow20WhenCalledTwice()
+        {
+            var sut = new Primes();
+
+            var _ = sut.GeneratePrimesBelow(10);
+            var primes = sut.GeneratePrimesBelow(20);
+
+            primes.ShouldBe((new int[] { 2, 3, 5, 7, 11, 13, 17, 19 }).ToList());
+        }
+
+        [Test]
+        [TestCase(2, new int[] { 1 })]
+        [TestCase(3, new int[] { 0, 1 })]
+        [TestCase(4, new int[] { 2, 0 })]
+        [TestCase(12, new int[] { 2, 1, 0, 0, 0 })]
+        [TestCase(15, new int[] { 0, 1, 1, 0, 0, 0 })]
+        public void ShouldFindPrimeFactorsForExamples(int input, int[] expectedResult)
+        {
+            var sut = new Primes();
+
+            var result = sut.FindPrimeFactors(input);
+
+            result.ShouldBe(expectedResult.ToList());
+        }
+    }
+}
---
+++ project-euler/project-euler/Problems/Maths/Primes.cs
@@ -0,0 +1,52 @@
+﻿namespace project_euler.Maths
+{
+    internal class Primes
+    {
+        private readonly List<int> knownPrimes = new();
+
+        public List<int> FindPrimeFactors(int num)
+        {
+            var rest = num;
+            var primes = GeneratePrimesBelow(num+1);
+            int index = 0;
+            var primeFactors = new int[primes.Count];
+            while(primes[index] <= rest)
+            {
+                if (IsDivisible(rest, primes[index]))
+                {
+                    rest /= primes[index];
+                    primeFactors[index]++;
+                }
+                else
+                {
+                    index++;
+                }
+            }
+            return primeFactors.ToList();
+        }
+
+        public List<int> GeneratePrimesBelow(int num)
+        {
+            List<int> primes = new(knownPrimes);
+            var startAt = 2;
+            if (knownPrimes.Count > 0)
+            {
+                startAt = knownPrimes.Last();
+            }
+            for (int i = startAt; i < num; i++)
+            {
+                if(primes.Exists(x => IsDivisible(i,x)))
+                {
+                    continue;
+                }
+                primes.Add(i);
+            }
+            return primes;
+        }
+
+        public static bool IsDivisible(int num, int divisor)
+        {
+            return num % divisor == 0;
+        }
+    }
+}
