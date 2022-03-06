dbc210577d4c598c3106d31e425562654846ac10
Asbjørn Brandt <asb@lindcapital.com>
2022-03-06 17:07:54 +0100

    Add hexdraft and initial tests

---
+++ project-euler/project-euler/Problems/Problem0128/Hex.cs
@@ -0,0 +1,27 @@
+﻿namespace project_euler.Problems.Problem0128
+{
+    internal class Hex
+    {
+        public int InternalNum { get; }
+        
+        //Neighbours
+        public Hex? XUp { get; set; }
+        public Hex? XDown { get; set; }
+        public Hex? YUp { get; set; }
+        public Hex? YDown { get; set; }
+        public Hex? ZUp { get; set; }
+        public Hex? ZDown { get; set; }
+
+        public Hex(int internalNum)
+        {
+            InternalNum = internalNum;
+        }
+
+        public List<Hex?> Neighbours => new() { XUp, XDown, YUp, YDown, ZUp, ZDown };
+
+        public bool IsInitialized => Neighbours.All(x => x is not null);
+
+        //Throws runtime nullpointerexception if not initialized
+        public List<int> GetDiffs => Neighbours.ConvertAll(x => Math.Abs(x!.InternalNum - InternalNum));
+    }
+}
---
+++ project-euler/project-euler/Problems/Problem0128/Problem0128a.cs
@@ -0,0 +1,12 @@
+﻿namespace project_euler.Problems.Problem0128
+{
+    internal class Problem0128a : BaseProblem, IProblem
+    {
+        public string Description => "Simple brute force solution";
+
+        public string Solve()
+        {
+            return "!";
+        }
+    }
+}
