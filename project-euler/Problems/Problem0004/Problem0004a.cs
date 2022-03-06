5fb71ba4bddcb0be9370a43be0af194f0d17a868
Asbjørn Brandt <asb@lindcapital.com>
2022-03-06 14:48:31 +0100

    Two inelegant solution for #4

---
+++ project-euler/Problems/Problem0004/Problem0004a.cs
@@ -0,0 +1,53 @@
+﻿namespace project_euler.Problems.Problem0004
+{
+    internal class Problem0004a : BaseProblem, IProblem
+    {
+        public string Description => "Simple brute force solution";
+
+        public string Solve()
+        {
+            return FindLargestPalindrome(10000).ToString();
+        }
+
+        private static int FindLargestPalindrome(int limit)
+        {
+            for (int i = limit - 1; i > 0; i--)
+            {
+                //Heuristicly this works, but other palindromes eixst that are not constructed like this (e.g. 121)
+                var asText = i.ToString();
+                var palindrome = asText + new string(asText.Reverse().ToArray());
+                var num = int.Parse(palindrome);
+                if (HasThreeDigitFactors(num))
+                {
+                    return num;
+                }
+            }
+            return 0;
+        }
+
+        private static bool HasThreeDigitFactors(int num)
+        {
+            for(int i = 100; i < 1000; i++)
+            {
+                if(i*i>num)
+                {
+                    return false;
+                }
+                if(IsDivisible(num, i))
+                {
+                    var otherFactor = num / i;
+                    if (otherFactor > 99 && otherFactor < 1000)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private static bool IsDivisible(int num, int divisor)
+        {
+            return num % divisor == 0;
+        }
+    }
+}
