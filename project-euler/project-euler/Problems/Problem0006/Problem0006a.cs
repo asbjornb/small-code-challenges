2992530669b32b133f588f185c0228505cbcf23c
Asbjørn Brandt <asb@lindcapital.com>
2022-03-07 08:42:36 +0100

    Add solution for #6

---
+++ project-euler/project-euler/Problems/Problem0006/Problem0006a.cs
@@ -0,0 +1,24 @@
+﻿namespace project_euler.Problems.Problem0005
+{
+    internal class Problem0006a : BaseProblem, IProblem
+    {
+        public string Description => "Simple brute force solution";
+
+        public string Solve()
+        {
+            return FindSumOfSquareDifference(100).ToString();
+        }
+
+        private static long FindSumOfSquareDifference(int n)
+        {
+            var sumOfSquares = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                sumOfSquares += i * i;
+            }
+            var sum = n * (n + 1) / 2;
+            var squaredSum = sum * sum;
+            return squaredSum- sumOfSquares;
+        }
+    }
+}
