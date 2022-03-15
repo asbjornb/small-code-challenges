b8516a47995d2fe22f5d193e05e336e7e4c6b4ec
Asbjørn Brandt <asb@lindcapital.com>
2022-03-15 21:58:30 +0100

    TempCommit doesn't finish

---
+++ project-euler/project-euler/Problems/ConcreteProblems/Problem012.cs
@@ -0,0 +1,35 @@
+﻿using project_euler.Maths.Primes;
+
+namespace project_euler.Problems.ConcreteProblems
+{
+    internal class Problem012 : BaseProblem, IProblem
+    {
+        public string Solve()
+        {
+            return FindFirstTriangleWithDivisors(500).ToString();
+        }
+
+        private static int FindFirstTriangleWithDivisors(int divisors)
+        {
+            var triangleNumber = 1;
+            var index = 1;
+            while (true)
+            {
+                index++;
+                triangleNumber += index;
+                if (NumDivisors(triangleNumber) >= 500) { return triangleNumber; }
+            }
+        }
+
+        private static int NumDivisors(int num)
+        {
+            var primeFactors = PrimesCalculator.FindPrimeFactors(num).ToList();
+            var divisors = 1;
+            foreach (var factor in primeFactors)
+            {
+                divisors *= (factor.Exponent + 1);
+            }
+            return divisors;
+        }
+    }
+}
