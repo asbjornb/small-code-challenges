9e167359717c2ea6bb2b60d368a6ac75fda48a19
Asbjørn Brandt <asb@lindcapital.com>
2022-03-16 08:11:18 +0100

    Add simple cache for expensive function calls

---
+++ project-euler/Tests/Utils/DictCacheTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using project_euler.Util;
+using Shouldly;
+using System;
+
+namespace Tests.Utils
+{
+    [TestFixture]
+    internal class DictCacheTests
+    {
+        [Test]
+        [TestCase(1)]
+        [TestCase(12)]
+        public void ShouldOnlyInvokeFirstTime(int testValue)
+        {
+            var sut = new DictCache<int,int>();
+            
+            var counter = 0;
+            Func<int,int> function = x => { counter++; return x; };
+
+            var result = sut.Get(testValue, function);
+            result.ShouldBe(testValue);
+            counter.ShouldBe(1);
+            
+            result = sut.Get(testValue, function);
+            result.ShouldBe(testValue);
+            counter.ShouldBe(1);
+
+            result = sut.Get(testValue+1, function);
+            result.ShouldBe(testValue+1);
+            counter.ShouldBe(2);
+        }
+    }
+}
---
+++ project-euler/project-euler/Utils/DictCache.cs
@@ -0,0 +1,18 @@
+﻿namespace project_euler.Util
+{
+    internal class DictCache<TKey,TValue> where TKey : struct, IEquatable<TKey>
+    {
+        private readonly Dictionary<TKey, TValue> dict=new();
+
+        public TValue Get(TKey key, Func<TKey, TValue> generator)
+        {
+            if (dict.ContainsKey(key))
+            {
+                return dict[key];
+            }
+            var value = generator(key);
+            dict.Add(key, value);
+            return value;
+        }
+    }
+}
