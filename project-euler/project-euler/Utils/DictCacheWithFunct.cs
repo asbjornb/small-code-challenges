9b08819f351ad8a89906bf6c5bb7f1b9453c89c4
Asbjørn Brandt <asb@lindcapital.com>
2022-03-16 08:24:41 +0100

    Add extension for DictCache with field for funct

---
+++ project-euler/project-euler/Utils/DictCacheWithFunct.cs
@@ -0,0 +1,24 @@
+﻿namespace project_euler.Util
+{
+    internal class DictCacheWithFunct<TKey,TValue> where TKey : struct, IEquatable<TKey>
+    {
+        private readonly Dictionary<TKey, TValue> dict=new();
+        private readonly Func<TKey, TValue> generator;
+
+        public DictCacheWithFunct(Func<TKey, TValue> generator)
+        {
+            this.generator = generator;
+        }
+
+        public TValue Get(TKey key)
+        {
+            if (dict.ContainsKey(key))
+            {
+                return dict[key];
+            }
+            var value = generator(key);
+            dict.Add(key, value);
+            return value;
+        }
+    }
+}
