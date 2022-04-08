f6287da867e723a4aa97e17a8613e37f5132e784
Asbjørn Brandt <asb@lindcapital.com>
2022-04-08 20:31:25 +0200

    Add initial perfect-sum-set

---
+++ project-euler/Tests/Maths/PerfectSumSetTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using project_euler.Maths.Sets;
+using Shouldly;
+using System.Linq;
+
+namespace Tests.Maths
+{
+    [TestFixture]
+    internal class PerfectSumSetTests
+    {
+        [Test]
+        [TestCase(new int[] { }, true)]
+        [TestCase(new int[] { 2 }, true)]
+        [TestCase(new int[] { 2, 3 }, true)]
+        [TestCase(new int[] { 1, 2, 3 }, false)]
+        [TestCase(new int[] { 6, 9, 11, 12, 13 }, true)]
+        [TestCase(new int[] { 6, 9, 11, 12, 13 }, true)]
+        [TestCase(new int[] { 11, 17, 20, 22, 23, 24 }, true)]
+        [TestCase(new int[] { 11, 18, 19, 20, 22, 25 }, true)]
+        [TestCase(new int[] { 11, 18, 19, 20, 22, 25 }, true)]
+        [TestCase(new int[] { 20, 31, 38, 39, 40, 42, 45 }, true)]
+        [TestCase(new int[] { 20, 31, 37, 39, 40, 42, 45 }, false)]
+        public void ShouldGenerateCorrectly(int[] candidateSet, bool expectedResult)
+        {
+            var sut = PerfectSumSet.Create();
+            var result = candidateSet.All(x => sut.Add(x));
+            
+            result.ShouldBe(expectedResult);
+        }
+    }
+}
---
+++ project-euler/project-euler/Maths/Sets/PerfectSumSet.cs
@@ -0,0 +1,55 @@
+﻿namespace project_euler.Maths.Sets
+{
+    internal sealed class PerfectSumSet
+    {
+        private readonly SortedSet<int> set;
+        private List<SortedSet<int>> subsets;
+
+        private PerfectSumSet()
+        {
+            set = new SortedSet<int>();
+            subsets = new List<SortedSet<int>>();
+        }
+
+        public static PerfectSumSet Create()
+        {
+            return new();
+        }
+
+        public bool Add(int i)
+        {
+            if(set.Count == 0)
+            {
+                set.Add(i);
+                subsets.Add(new SortedSet<int> { i });
+                return true;
+            }
+            var candidateSubsets = new List<SortedSet<int>>(subsets);
+            candidateSubsets.Add(new SortedSet<int> { i });
+            candidateSubsets.AddRange(subsets.Select(x => new SortedSet<int>(x) { i }));
+            if (Validate(candidateSubsets))
+            {
+                set.Add(i);
+                subsets = candidateSubsets;
+                return true;
+            }
+            return false;
+        }
+
+        //Should use existing pairs to not re-validate
+        private static bool Validate(List<SortedSet<int>> candidateSubsets)
+        {
+            var combinations = candidateSubsets.SelectMany(_=>candidateSubsets, (x,y)=>(x,y))
+                .Where(pair => pair.x!=pair.y);
+            //Can be made more efficient with .Where((x,y) => x<y) with some ordering
+            //Also add compare - so new class. Need to filter combinations for B!=C.
+            if(combinations.Any(pair=>pair.x.Sum()== pair.y.Sum())){
+                return false;
+            }
+            if (combinations.Any(pair => pair.x.Sum() >= pair.y.Sum() && pair.x.Count < pair.y.Count)){
+                return false;
+            }
+            return true;
+        }
+    }
+}
