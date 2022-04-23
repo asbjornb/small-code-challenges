d108c3490fbe9d9869323ee54733f87e96d743c4
Asbjørn Brandt <asb@lindcapital.com>
2022-04-23 10:29:24 +0200

    Avoid recalculating F(m,n)

--- project-euler/project-euler/Problems/ConcreteProblems/Problem115.cs
+++ project-euler/project-euler/Problems/ConcreteProblems/Problem115.cs
@@ -7,37 +7,28 @@
             return FindMinLengthOver(1000000).ToString();
         }
 
-        private static int FindMinLengthOver(int v)
+        private static int FindMinLengthOver(int limit)
         {
-            var length = 101; //At least two can be placed
+            var length = 1; //
+            const int minSequence = 50;
+            var pathsToState = new int[minSequence + 1];
+            pathsToState[0] = 1;
             while (true)
             {
-                if (CountCombinations(50, length) >= v)
+                var stateZeroes = pathsToState[0] + pathsToState[minSequence];
+                var stateMs = pathsToState[minSequence - 1] + pathsToState[minSequence];
+                if (stateZeroes + stateMs > limit)
+                {
                     return length;
-                length++;
-            }
-        }
-
-        //See 114 for simple example
-        private static int CountCombinations(int m, int n)
-        {
-            //Traversing the length we are always in 1 of 4 states depending on the number of preceding reds.
-            //With 0 or 3+ preceding reds we can make the new block red or grey.
-            //With 1 or 2 preceding reds we can only make the new block red.
-            var pathsToState = new int[m+1];
-            pathsToState[0] = 1;
-            for (int i = 0; i < n; i++)
-            {
-                var stateZeroes = pathsToState[0]+pathsToState[m];
-                var stateMs = pathsToState[m-1]+ pathsToState[m];
-                for (int j = m-1; j > 0; j--)
+                }
+                for (int j = minSequence - 1; j > 0; j--)
                 {
                     pathsToState[j] = pathsToState[j - 1];
                 }
                 pathsToState[0] = stateZeroes;
-                pathsToState[m] = stateMs;
+                pathsToState[minSequence] = stateMs;
+                length++;
             }
-            return pathsToState[0] + pathsToState[m];
         }
     }
 }
