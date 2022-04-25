52069ec2d7938cd808d34fd32bca3323a47eab76
Asbjørn Brandt <asb@lindcapital.com>
2022-04-25 21:42:57 +0200

    Small refinement and idea for faster solution

--- project-euler/project-euler/Problems/ConcreteProblems/Problem030.cs
+++ project-euler/project-euler/Problems/ConcreteProblems/Problem030.cs
@@ -1,16 +1,25 @@
-﻿using System.Numerics;
-
-namespace project_euler.Problems.ConcreteProblems
+﻿namespace project_euler.Problems.ConcreteProblems
 {
     internal class Problem030 : BaseProblem, IProblem
     {
+        private static readonly Dictionary<int, int> powerDict = new();
         public string Solve()
         {
+            PopulateDict();
             return FindDigitFifthPowers().ToString();
         }
-        
+
         //Notes:
-        //A naive brute force approach
+        //A naive brute force approach for now. To optimize consider:
+        //Also permutations have same digit sum so should only be checked once - 6 forloops to 10 should be faster.
+        private void PopulateDict()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                powerDict.Add(i, (int)Math.Pow(i, 5));
+            }
+        }
+
         private static double FindDigitFifthPowers()
         {
             var sum = 0;
@@ -28,7 +37,7 @@ namespace project_euler.Problems.ConcreteProblems
         private static bool IsDigitFifthPower(int i)
         {
             var asDigits = i.ToString().Select(x => x-'0');
-            return i == asDigits.Sum(x => (int)Math.Pow(x, 5));
+            return i == asDigits.Sum(x => powerDict.GetValueOrDefault(x, 0));
         }
     }
 }
