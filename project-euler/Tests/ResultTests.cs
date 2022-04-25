ef47297a0a7ca312b1302301e7b3228fb3a9037c
Asbjørn Brandt <asb@lindcapital.com>
2022-04-25 21:07:16 +0200

    Add solution for #28

--- project-euler/Tests/ResultTests.cs
+++ project-euler/Tests/ResultTests.cs
@@ -37,6 +37,7 @@ namespace Tests
         [TestCase("025", "4782")]
         [TestCase("026", "983")]
         [TestCase("027", "-59231")]
+        [TestCase("028", "669171001")]
         [TestCase("067", "7273")]
         [TestCase("103", "20313839404245")]
         [TestCase("105", "73702")]
---
+++ project-euler/project-euler/Problems/ConcreteProblems/Problem028.cs
@@ -0,0 +1,27 @@
+﻿namespace project_euler.Problems.ConcreteProblems
+{
+    internal class Problem028 : BaseProblem, IProblem
+    {
+        public string Solve()
+        {
+            return FindCornerSum().ToString();
+        }
+
+        //Notes:
+        //Observe that moving between corners just adds the same number to each corner and moving out a layer adds 2 to height/width.
+        private static double FindCornerSum()
+        {
+            var cornerSum = 1d; //layer 1 has sum 1 so we start from 2
+            var increment = 1;
+            for (int layer = 2; layer <= 501; layer++) //layer 501 has 1001 height/width
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    increment += 2 * (layer - 1);
+                    cornerSum += increment;
+                }
+            }
+            return cornerSum;
+        }
+    }
+}
