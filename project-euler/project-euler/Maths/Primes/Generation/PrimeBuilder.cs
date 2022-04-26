1ed73cf66e62e023dcd04c8bf06aa9d4171121fc
Asbjørn Brandt <asb@lindcapital.com>
2022-04-26 23:04:29 +0200

    Change PrimeBuilder to yield return

--- project-euler/project-euler/Maths/Primes/Generation/PrimeBuilder.cs
+++ project-euler/project-euler/Maths/Primes/Generation/PrimeBuilder.cs
@@ -4,32 +4,33 @@
     {
         public static IEnumerable<int> BuildPrimesBelow(int num)
         {
-            var composites = CompositesBelow(num);
-            var primesWithIndex = composites.Select((isNotPrime, index) => (isPrime: !isNotPrime, number: index));
-            return primesWithIndex.Where(x => x.isPrime).Select(x => x.number);
-        }
-
-        private static bool[] CompositesBelow(int num)
-        {
             if (num < 3)
             {
                 throw new ArgumentException("Use only with num>=3");
             }
+            //Marking notPrimes is easier since bool defaults to false
             bool[] notPrime = new bool[num];
-            var limit = (int)Math.Sqrt(notPrime.Length);
-            notPrime[0] = true;
+            var limit = (int)Math.Sqrt(num);
+            notPrime[0] = true; //These don't need to be marked as prime, but why not?
             notPrime[1] = true;
             for (int i = 2; i <= limit; i++)
             {
                 if (!notPrime[i])
                 {
-                    for (int j = i * i; j < notPrime.Length; j += i)
+                    for (int j = i * i; j < num; j += i)
                     {
                         notPrime[j] = true;
                     }
+                    yield return i;
                 }
             }
-            return notPrime;
+            for (int i = limit+1; i <num; i++)
+            {
+                if (!notPrime[i])
+                {
+                    yield return i;
+                }
+            }
         }
     }
 }
