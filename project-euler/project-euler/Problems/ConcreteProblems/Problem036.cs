9a72aef8b9f2391f7e712875178643806fe066e7
Asbjørn Brandt <asb@lindcapital.com>
2022-05-02 23:49:53 +0200

    Improve #36 by only iterating palindromes

--- project-euler/project-euler/Problems/ConcreteProblems/Problem036.cs
+++ project-euler/project-euler/Problems/ConcreteProblems/Problem036.cs
@@ -4,21 +4,30 @@
     {
         public string Solve()
         {
-            return SumDoubleBasedPalindromesBelow(1000000).ToString();
+            return SumDoubleBasedPalindromesBelow().ToString();
         }
 
-        private static int SumDoubleBasedPalindromesBelow(int limit)
+        private static int SumDoubleBasedPalindromesBelow()
         {
             var sum = 0;
             //loop to 1000 instead and generate palindromes as p=i+reverse(i) and p=i+reverse(i).Skip(1)?
-            for (int i = 1; i < limit; i++)
+            for (int i = 1; i < 1000; i++)
             {
                 var numBase10 = Convert.ToString(i, 10);
-                var numBase2 = Convert.ToString(i, 2);
-                if (numBase10.Select(x=>x).SequenceEqual(numBase10.Reverse())
-                    && numBase2.Select(x => x).SequenceEqual(numBase2.Reverse()))
+                string test = new string(numBase10.Reverse().ToArray());
+                string palindromeString1 = (numBase10 + new string(numBase10.Reverse().ToArray()));
+                var palindromeNum1 = Convert.ToInt32(palindromeString1, 10);
+                var possiblePalindrome1 = Convert.ToString(palindromeNum1, 2);
+                string palindromeString2 = (numBase10 + new string(numBase10.Reverse().Skip(1).ToArray()));
+                var palindromeNum2 = Convert.ToInt32(palindromeString2, 10);
+                var possiblePalindrome2 = Convert.ToString(palindromeNum2, 2);
+                if (possiblePalindrome1.Select(x=>x).SequenceEqual(possiblePalindrome1.Reverse()))
                 {
-                    sum += i;
+                    sum += palindromeNum1;
+                }
+                if (possiblePalindrome2.Select(x => x).SequenceEqual(possiblePalindrome2.Reverse()))
+                {
+                    sum += palindromeNum2;
                 }
             }
             return sum;
