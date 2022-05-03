4c77c01746920292e19886471baabfe533b8ca20
Asbjørn Brandt <asb@lindcapital.com>
2022-05-03 11:02:20 +0200

    Add shortcircuit in #35 to improve time

--- project-euler/project-euler/Problems/ConcreteProblems/Problem035.cs
+++ project-euler/project-euler/Problems/ConcreteProblems/Problem035.cs
@@ -27,7 +27,12 @@ namespace project_euler.Problems.ConcreteProblems
 
         private static bool IsCircularPrime(int candidate)
         {
-            foreach (var rotation in GetRotations(candidate))
+            var digits = Digits.GetDigits(candidate);
+            if(digits.Count>1 && HasOutlawed(digits)) // Short circuit for numbers known not to be circular primes
+            {
+                return false;
+            }
+            foreach (var rotation in GetRotations(digits))
             {
                 if (!PrimesCalculator.IsPrime(Digits.ToNum(rotation)))
                 {
@@ -37,9 +42,27 @@ namespace project_euler.Problems.ConcreteProblems
             return true;
         }
 
-        private static IEnumerable<IEnumerable<int>> GetRotations(int candidate)
+        // If the prime contains a 2, 4, 5, 6, 8, or 0 then there will be a rotation ending in that digit.
+        // Numbers ending in 5 are devisible by 5, even numbers are divisible by 2.
+        private static bool HasOutlawed(List<int> digits)
         {
-            var digits = Digits.GetDigits(candidate);
+            foreach(var digit in digits)
+            {
+                if (digit == 0
+                    || digit == 2
+                    || digit == 4
+                    || digit == 5
+                    || digit == 6
+                    || digit == 8)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static IEnumerable<IEnumerable<int>> GetRotations(List<int> digits)
+        {
             IEnumerable<int> next = digits;
             for (int i = 0; i < digits.Count-1; i++)
             {
