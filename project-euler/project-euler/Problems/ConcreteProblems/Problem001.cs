499d2029f684a364be41fe5cb48669613fdcf561
Asbjørn Brandt <asb@lindcapital.com>
2022-05-16 21:51:16 +0200

    Specialise #1

--- project-euler/project-euler/Problems/ConcreteProblems/Problem001.cs
+++ project-euler/project-euler/Problems/ConcreteProblems/Problem001.cs
@@ -4,37 +4,20 @@
     {
         public string Solve()
         {
-            return SumMultiplesUnder(new List<int>() { 3, 5 }, 1000).ToString();
+            return SumMultiplesUnder(1000).ToString();
         }
 
-        private static int SumMultiplesUnder(List<int> divisors, int limit)
+        private static int SumMultiplesUnder(int limit)
         {
             int sum = 0;
             for (int i = 0; i < limit; i++)
             {
-                if (IsDivisible(i, divisors))
+                if (i % 3 == 0 || i % 5 == 0)
                 {
                     sum += i;
                 }
             }
             return sum;
-        }
-
-        private static bool IsDivisible(int num, List<int> divisors)
-        {
-            foreach (var divisor in divisors)
-            {
-                if (IsDivisible(num, divisor))
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        private static bool IsDivisible(int num, int divisor)
-        {
-            return num % divisor == 0;
         }
     }
 }
