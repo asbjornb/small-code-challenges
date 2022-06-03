c1b93a402391c997aacfada53375d2487a1767de
Asbjørn Brandt <asb@lindcapital.com>
2022-06-03 22:36:23 +0200

    Redo lc #53

--- python-leetcode/053-maximum-subarray.py
+++ python-leetcode/053-maximum-subarray.py
@@ -3,10 +3,13 @@
 from typing import List
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+        max = nums[0]
         for i in range(1, len(nums)):
-            if nums[i-1] > 0:
-                nums[i] = nums[i-1]+nums[i]
-        return max(nums)
+            if(nums[i-1] > 0):
+                nums[i]+=nums[i-1]
+            if(nums[i] > max):
+                max = nums[i]
+        return max
 
 sol = Solution()
 print(sol.maxSubArray([-2,1,-3,4,-1,2,1,-5,4]), "should be 6")
