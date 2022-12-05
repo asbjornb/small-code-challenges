7c2b16ab344dae86b14eee8a98dbf72d2a1f05d1
Asbjørn Brandt <asb@lindcapital.com>
2022-12-05 12:35:45 +0100

    Refactor to enable part 2

--- advent-of-code/AdventOfCode/Solutions/Day01.fs
+++ advent-of-code/AdventOfCode/Solutions/Day01.fs
@@ -8,12 +8,12 @@ let readInput path = File.ReadAllText path
 //take a string, split it into a list of strings at each newline
 let split (input:string) = input.Split("\n")
 
-//Couple it all together to find the largest group sum
-let largestGroupSum (input:string) = 
+let groupSums (input:string) =
     input.Split("\n\n") //group items between each empty line
     |> Array.map split //for each group split again at newlines
     |> Array.map (Array.map int) //map each individual string to ints
     |> Array.map Array.sum
-    |> Array.max
 
-let runDay01Part1 = readInput "Input/day01.txt" |> largestGroupSum
+let runDay01Part1 = readInput "Input/day01.txt" |> groupSums |> Array.max
+
+
